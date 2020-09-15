4569d14e4f79718e7ee13280fba4fc205ae6723c
Javed Gangjee <jgangjee@gmail.com>
2020-09-15 15:21:44 -0400

    Added screenshots. Logo

--- src/components/TTSButton.js
+++ src/components/TTSButton.js
@@ -9,15 +9,14 @@ import Tts from 'react-native-tts';
 
 
 export default class TTSButton extends Component {
-    speak = (speakText) => {
 
-    }
     render() {
         return (
             <View style={{ flexGrow: 1 }} >
                 <Button full disabled={this.props.disabled} onChange={this.props.onChange}
-                    style={{ margin: this.props.margin, flexGrow: 1, backgroundColor: this.props.backgroundColor }}
+                 style={{ margin: this.props.margin, flexGrow: 1, backgroundColor: this.props.backgroundColor }}
                     onPress={() => {
+                        Tts.voices().then(voices => console.log(voices));
                         Tts.speak(this.props.textValue)
                     }} >
                     <Text style={{ fontSize: this.props.fontSize, color: this.props.fontColor }}>
